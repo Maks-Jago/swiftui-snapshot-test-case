290a49e00091f1e0253228c2bbf747f164d2074b
Max Kuznetsov <mx.kuznetsov.dev@gmail.com>
2021-11-28 18:59:48 +0200

    ViewImageConfig

--- Sources/SwiftUISnapshotTestCase/SnapshotTestCase.swift
+++ Sources/SwiftUISnapshotTestCase/SnapshotTestCase.swift
@@ -1,11 +1,13 @@
 import Foundation
 import XCTest
 import SnapshotTesting
+@_exported import struct SnapshotTesting.ViewImageConfig
 import SwiftUI
 
 open class SnapshotTestCase: XCTestCase {
     open var isRecording: Bool = false
-    open var devices: [DeviceSize] = [.iPhone13ProMaxAndiPhone12ProMax, .iPhones13Pro_13_12Pro_12, .iPhone13miniAnd12mini, .iPhonesSE2_8_7_6S, .iPhoneSEAndiPodTouch7]
+//    open var devices: [DeviceSize] = [.iPhone13ProMaxAndiPhone12ProMax, .iPhones13Pro_13_12Pro_12, .iPhone13miniAnd12mini, .iPhonesSE2_8_7_6S, .iPhoneSEAndiPodTouch7]
+    open var devices: [ViewImageConfig] = [.iPhoneXsMax, .iPhone8, .iPhoneSe, .iPhone8Plus]
 
     public func snapshot<V: View>(for component: V, colorScheme: ColorScheme = .light, file: StaticString = #file, testName: String = #function, line: UInt = #line) {
         let view = component
@@ -13,20 +15,27 @@ open class SnapshotTestCase: XCTestCase {
             .preferredColorScheme(colorScheme)
 
         devices.forEach { deviceSize in
-            validateOrRecord(for: vc(for: view, device: deviceSize), scale: deviceSize.scaleFactor, file: file, testName: testName + "_" + deviceSize.rawValue, line: line)
+            validateOrRecord(for: UIHostingController(rootView: view), config: deviceSize, file: file, testName: testName + "_\(deviceSize)", line: line)
         }
     }
 
-    private func validateOrRecord<V: View>(for component: UIHostingController<V>, scale: CGFloat, file: StaticString, testName: String, line: UInt) {
+//    private func validateOrRecord<V: View>(for component: UIHostingController<V>, scale: CGFloat, file: StaticString, testName: String, line: UInt) {
+//        let bundlePath = Bundle(for: type(of: self)).bundlePath
+////        assertSnapshot(matching: component, as: .image(scale: scale), record: self.isRecording, snapshotDirectory: bundlePath, file: file, testName: testName, line: line)
+//        assertSnapshot(matching: component, as: .image(on: .iPadPro12_9), record: self.isRecording, snapshotDirectory: bundlePath, file: file, testName: testName, line: line)
+//    }
+
+    private func validateOrRecord<V: View>(for component: UIHostingController<V>, config: ViewImageConfig, file: StaticString, testName: String, line: UInt) {
         let bundlePath = Bundle(for: type(of: self)).bundlePath
-        assertSnapshot(matching: component, as: .image(scale: scale), record: self.isRecording, snapshotDirectory: bundlePath, file: file, testName: testName, line: line)
+        //        assertSnapshot(matching: component, as: .image(scale: scale), record: self.isRecording, snapshotDirectory: bundlePath, file: file, testName: testName, line: line)
+        assertSnapshot(matching: component, as: .image(on: config), record: self.isRecording, snapshotDirectory: bundlePath, file: file, testName: testName, line: line)
     }
 
-    private func vc<V: View>(for view: V, device: DeviceSize) -> UIHostingController<V> {
-        let vc = UIHostingController(rootView: view)
-        vc.view.frame = .init(x: 0, y: 0, width: device.size.width, height: device.size.height)
-        return vc
-    }
+//    private func vc<V: View>(for view: V, device: DeviceSize) -> UIHostingController<V> {
+//        let vc = UIHostingController(rootView: view)
+////        vc.view.frame = .init(x: 0, y: 0, width: device.size.width, height: device.size.height)
+//        return vc
+//    }
 }
 
 //fileprivate extension DeviceSize {
