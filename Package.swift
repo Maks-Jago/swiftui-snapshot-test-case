3de33dda55971328f988521839996c6042bfd536
Max Kuznetsov <mx.kuznetsov.dev@gmail.com>
2021-12-17 22:16:00 +0200

    1.9.32

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["SwiftUISnapshotTestCase"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/urlaunched-com/swift-snapshot-testing.git", .exact("1.9.31"))
+        .package(url: "https://github.com/urlaunched-com/swift-snapshot-testing.git", .exact("1.9.32"))
     ],
     targets: [
         .target(
