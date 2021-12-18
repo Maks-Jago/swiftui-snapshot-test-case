3ae70c13302d259f15256004197ac4107360aaa2
Max Kuznetsov <mx.kuznetsov.dev@gmail.com>
2021-12-18 22:01:38 +0200

    1.9.42

--- Sources/SwiftUISnapshotTestCase/SnapshotTestCase.swift
+++ Sources/SwiftUISnapshotTestCase/SnapshotTestCase.swift
@@ -13,17 +13,44 @@ open class SnapshotTestCase: XCTestCase {
             .environment(\.colorScheme, colorScheme)
             .preferredColorScheme(colorScheme)
 
+        var interfaceStyle: UIUserInterfaceStyle {
+            switch colorScheme {
+            case .light:
+                return .light
+            case .dark:
+                return .dark
+            @unknown default:
+                return .light
+            }
+        }
+
         devices.forEach { deviceSize in
             let vc = SnapshotHostingController(rootView: view, insets: deviceSize.safeArea)
-            validateOrRecord(for: vc, config: deviceSize, precision: precision, file: file, testName: testName + "_" + deviceSize.name, line: line)
+            validateOrRecord(
+                for: vc,
+                config: deviceSize,
+                precision: precision,
+                interfaceStyle: interfaceStyle,
+                file: file,
+                testName: testName + "_" + deviceSize.name,
+                line: line
+            )
         }
     }
 
-    private func validateOrRecord<V: View>(for component: UIHostingController<V>, config: ViewImageConfig, precision: Float, file: StaticString, testName: String, line: UInt) {
+    private func validateOrRecord<V: View>(
+        for component: UIHostingController<V>,
+        config: ViewImageConfig,
+        precision: Float,
+        interfaceStyle: UIUserInterfaceStyle,
+        file: StaticString,
+        testName: String,
+        line: UInt
+    ) {
         let bundlePath = Bundle(for: type(of: self)).bundlePath
         assertSnapshot(
             matching: component,
-            as: .image(on: config, precision: precision),
+            as: .image(on: config, precision: precision, interfaceStyle: interfaceStyle),
             record: self.isRecording,
             snapshotDirectory: bundlePath,
             file: file,
