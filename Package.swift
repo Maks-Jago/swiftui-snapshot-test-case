d0446259d6ad68f3f5e61bd11b19440c7d8cedb4
Max Kuznetsov <mx.kuznetsov.dev@gmail.com>
2022-08-13 13:02:54 +0300

    endAppearanceTransition

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
 //        .package(url: "https://github.com/urlaunched-com/swift-snapshot-testing.git", from: "2.1.0")
-        .package(url: "https://github.com/urlaunched-com/swift-snapshot-testing.git", revision: "e2241417c0b77867060a389bfb8f890081f7c007")
+        .package(url: "https://github.com/urlaunched-com/swift-snapshot-testing.git", revision: "ee262c0d0de4de328ba2e0c6ff9419fe4b02305b")
     ],
     targets: [
         .target(
