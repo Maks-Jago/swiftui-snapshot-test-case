6b847438b7f132969af7440886c960dfe93d34c0
Max Kuznetsov <mx.kuznetsov.dev@gmail.com>
2022-10-18 12:50:43 +0300

    swift-snapshot-testing 2.3.0

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
             targets: ["SwiftUISnapshotTestCase"]),
     ],
     dependencies: [
-//        .package(url: "https://github.com/urlaunched-com/swift-snapshot-testing.git", from: "2.2.2")
-        .package(url: "https://github.com/urlaunched-com/swift-snapshot-testing.git", revision: "3252bccebc12c2ad48557fc71f8e9af5f47dfa62")
+        .package(url: "https://github.com/urlaunched-com/swift-snapshot-testing.git", from: "2.3.0")
+//        .package(url: "https://github.com/urlaunched-com/swift-snapshot-testing.git", revision: "3252bccebc12c2ad48557fc71f8e9af5f47dfa62")
     ],
     targets: [
         .target(
