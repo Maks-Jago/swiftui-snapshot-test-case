3d101afee81b17cc84b1aeab09b416d8b82d222f
Max Kuznetsov <mx.kuznetsov.dev@gmail.com>
2024-09-11 17:36:02 +0300

    delayForLayout

--- Sources/SwiftUISnapshotTestCase/SnapshotTestCase.swift
+++ Sources/SwiftUISnapshotTestCase/SnapshotTestCase.swift
@@ -8,6 +8,7 @@ open class SnapshotTestCase: XCTestCase {
     open var isRecording: Bool = false
     open var devices: [ViewImageConfig] = [.iPhone13Pro]
     public static var deviceReference: String = "iPhone 15 Pro"
+    public static var osVersionReference: String = "17.5"
 
     private var recordMode: SnapshotTestingConfiguration.Record {
         isRecording ? .all : .missing
@@ -16,7 +17,12 @@ open class SnapshotTestCase: XCTestCase {
     open override class func setUp() {
         let device = UIDevice.current.name
         if device != deviceReference {
-            fatalError("Switch to using iPhone 15 Pro for these tests.")
+            fatalError("Switch to using \(deviceReference) (\(osVersionReference) for these tests.")
+        }
+
+        let systemVersion = UIDevice.current.systemVersion
+        if systemVersion != osVersionReference {
+            fatalError("Switch to using \(deviceReference) (\(osVersionReference) for these tests.")
         }
 
         UIView.setAnimationsEnabled(false)
@@ -27,6 +33,7 @@ open class SnapshotTestCase: XCTestCase {
         for view: some View,
         precision: Float = 0.99,
         perceptualPrecision: Float = 0.98,
+        delayForLayout: TimeInterval = 0,
         file: StaticString = #file,
         testName: String = #function,
         line: UInt = #line,
@@ -35,18 +42,36 @@ open class SnapshotTestCase: XCTestCase {
         let vc = UIHostingController(rootView: view)
         withSnapshotTesting(record: recordMode) {
             for device in devices {
-                assertSnapshot(
-                    of: vc,
-                    as: .image(
-                        on: device,
-                        precision: precision,
-                        perceptualPrecision: perceptualPrecision
-                    ),
-                    file: file,
-                    testName: testName,
-                    line: line,
-                    column: column
-                )
+                if delayForLayout > 0 {
+                    assertSnapshot(
+                        of: vc,
+                        as: .wait(
+                            for: delayForLayout,
+                            on: .image(
+                                on: device,
+                                precision: precision,
+                                perceptualPrecision: perceptualPrecision
+                            )
+                        ),
+                        file: file,
+                        testName: testName,
+                        line: line,
+                        column: column
+                    )
+                } else {
+                    assertSnapshot(
+                        of: vc,
+                        as: .image(
+                            on: device,
+                            precision: precision,
+                            perceptualPrecision: perceptualPrecision
+                        ),
+                        file: file,
+                        testName: testName,
+                        line: line,
+                        column: column
+                    )
+                }
             }
         }
     }
