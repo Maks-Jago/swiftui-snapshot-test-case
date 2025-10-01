56bec3c1f2c0a93248122298eec35c90b2a2971b
Max Kuznetsov <mx.kuznetsov.dev@gmail.com>
2025-10-01 18:48:00 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "SwiftUISnapshotTestCase",
     platforms: [
-        .iOS(.v14),
-        .macOS(.v11)
+        .iOS(.v16),
+        .macOS(.v12)
     ],
     products: [
         .library(
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["SwiftUISnapshotTestCase"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.17.4")
+        .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.18.7")
 //        .package(url: "https://github.com/urlaunched-com/swift-snapshot-testing.git", from: "2.3.6")
 //        .package(url: "https://github.com/urlaunched-com/swift-snapshot-testing.git", revision: "423e38673ac723e975b890cd86b99922e8de0ad4")
     ],
